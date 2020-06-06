d78ff66f3442ba75c07cd924de2d35533ca253a5
obinna Emelumadu <39995754+obinnaEmelumadu@users.noreply.github.com>
2020-06-06 03:07:28 +0100

    Save to csv
    Failed due to read only file system

--- app/src/main/java/com/example/assessmentandroid/CarActivity.java
+++ app/src/main/java/com/example/assessmentandroid/CarActivity.java
@@ -1,33 +1,34 @@
 package com.example.assessmentandroid;
 
-import androidx.annotation.IdRes;
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.icu.util.Calendar;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.annotation.SuppressLint;
-import android.app.ProgressDialog;
-import android.os.AsyncTask;
-import android.os.Build;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-
 import com.example.assessmentandroid.filter.CarItem;
 import com.google.gson.Gson;
 
-import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.StringWriter;
-import java.io.UnsupportedEncodingException;
-import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+import de.siegmar.fastcsv.writer.CsvAppender;
+import de.siegmar.fastcsv.writer.CsvWriter;
 
 public class CarActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
@@ -68,7 +69,7 @@ public class CarActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.action_filter:
+            case R.id.action_save:
                 CarActivity.SaveDataAsyncTasks saveDataAsyncTasks = new CarActivity.SaveDataAsyncTasks();
                 saveDataAsyncTasks.execute();
                 break;
@@ -81,6 +82,52 @@ public class CarActivity extends AppCompatActivity {
         @RequiresApi(api = Build.VERSION_CODES.O)
         @Override
         protected String doInBackground(String... params) {
+            File docsFolder = new File(Environment.getExternalStorageDirectory() + "/Decagon");
+            boolean isPresent = true;
+            if (!docsFolder.exists()) {
+                isPresent = docsFolder.mkdir();
+            }
+
+            File file = null;
+            try {
+                if (isPresent) {
+                    file = new File(docsFolder.getAbsolutePath(), "file.csv");
+                }
+                else{
+                    throw new  IOException();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                file = new File( "file.csv");
+            }
+
+            CsvWriter csvWriter = new CsvWriter();
+
+            try (CsvAppender csvAppender = csvWriter.append(file, StandardCharsets.UTF_8)) {
+                // header
+                csvAppender.appendLine("id",
+                        "first_name", "last_name", "email",
+                        "country", "car_model", "car_model_year",
+                        "car_color", "gender", "job_title", "bio");
+
+                // 1st line in one operation
+                int i = 1;
+                for (CarItem car : mDataset) {
+                    csvAppender.appendLine(String.valueOf(i),
+                            car.first_name, car.last_name, car.email,
+                            car.country, car.car_model, car.car_model_year,
+                            car.car_color, car.gender, car.job_title, car.bio);
+                    i++;
+                }
+                csvAppender.endLine();
+
+                Toast.makeText(getBaseContext(), "File saved successfully!",
+                        Toast.LENGTH_SHORT).show();
+            } catch (IOException e) {
+                e.printStackTrace();
+
+
+            }
             return "Executed";
         }
 
@@ -88,7 +135,7 @@ public class CarActivity extends AppCompatActivity {
         protected void onPreExecute() {
             super.onPreExecute();
             progressDialog = new ProgressDialog(CarActivity.this);
-            progressDialog.setMessage("Caching CSV, Please Wait");
+            progressDialog.setMessage("Saving CSV, Please Wait");
             progressDialog.setCancelable(false);
             progressDialog.show();
         }
